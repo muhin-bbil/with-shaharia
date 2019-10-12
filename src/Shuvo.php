f0bc6fa63c77152ee5a5e49ba21c30d1b0f52cdc
Saeed <saeed@blubirdinteractive.com>
2019-10-12 16:36:10 +0600

    Shuvo class added

---
+++ src/Shuvo.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Muhin\WithShaharia;
+
+
+class Shuvo implements AlbumInterface
+{
+
+    public function getThumbnail()
+    {
+        $thumbnails = array(
+            "https://images.unsplash.com/photo-1500322969630-a26ab6eb64cc?ixlib=rb-1.2.1&w=1000&q=80",
+            "https://i.ytimg.com/vi/BgIJ45HKDpw/maxresdefault.jpg"
+        );
+        return $thumbnails;
+    }
+
+    public function getPublishedAt()
+    {
+        $publishedAt = array(
+            "4 minutes ago",
+            "5 minutes ago"
+        );
+        return $publishedAt;
+    }
+
+    public function getDescription()
+    {
+        $descriptions = array(
+            "Description 1",
+            "Description 2",
+        );
+        return $descriptions;
+    }
+}
