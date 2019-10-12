c8f841d7edb85f022e8720d61a40d14dab4f0ac7
rizve <rizve@blubirdinteractive.com>
2019-10-12 16:33:06 +0600

    RIzve commit

---
+++ src/Rizve.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Muhin\WithShaharia;
+
+
+class Rizve implements AlbumInterface
+{
+
+    public function getThumbnail()
+    {
+        // TODO: Implement getThumbnail() method.
+    }
+
+    public function getPublishedAt()
+    {
+        // TODO: Implement getPublishedAt() method.
+    }
+
+    public function getDescription()
+    {
+        // TODO: Implement getDescription() method.
+    }
+}
